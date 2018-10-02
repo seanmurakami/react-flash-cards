821639b3067b92e844f6c2a0453b59971df3d3ca
Sean Murakami <murakami.sean@yahoo.com>
2018-10-02 15:55:06 -0700

    #1 updated the CreateCard react function to a class && gave it a state with properties "question" and "answer" && created two function "handleSave" and "handleChange" that update the state and set the state to local storage after the user clicks the "save" button

--- src/app.js
+++ src/app.js
@@ -6,25 +6,6 @@ export default class App extends React.Component {
     super(props)
     this.state = {}
   }
-  componentDidMount() {
-    document.querySelector('#createFlash').addEventListener('submit', (e) => {
-      e.preventDefault()
-      const flashcard = {
-        question: document.querySelector('[type=question]').value,
-        answer: document.querySelector('[type=answer]').value
-      }
-      const { question, answer } = flashcard
-      if (!question || !answer) {
-        alert('Please enter content in both fields')
-      }
-      else {
-        const flashCards = JSON.parse(localStorage.getItem('flashCards')) || []
-        flashCards.push(flashcard)
-        localStorage.setItem('flashCards', JSON.stringify(flashCards))
-        e.target.reset()
-      }
-    })
-  }
   render() {
     return (
       <CreateCard />
--- src/create-card.js
+++ src/create-card.js
@@ -2,27 +2,67 @@ import React from 'react'
 
 const styles = {
   background: {
-    backgroundColor: '#fffbe0',
     width: '32rem'
   }
 }
 
-export default function CreateCard() {
-  return (
-    <div className="d-flex justify-content-center">
-      <form id="createFlash" className="border border-dark rounded mt-5 p-4" style={styles.background}>
-        <h2 className="d-flex justify-content-center">Create a Flash Card</h2>
-        <div className="form-group">
-          <label>Question</label>
-          <input type="question" className="form-control" placeholder="Enter question"></input>
-        </div>
-        <div className="form-group">
-          <label>Answer</label>
-          <input type="answer" className="form-control" placeholder="Enter answer"></input>
-        </div>
-        <div className="d-flex justify-content-center">
-          <button type="submit" className="btn btn-primary">Save</button>
-        </div>
-      </form>
-  </div>)
+export default class CreateCard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      question: '',
+      answer: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSave = this.handleSave.bind(this)
+  }
+  handleSave(e) {
+    e.preventDefault()
+    const { question, answer } = this.state
+    if (!question || !answer) {
+      alert('Please enter content in both fields')
+    }
+    else {
+      const flashCards = JSON.parse(localStorage.getItem('flashCards')) || []
+      flashCards.push(this.state)
+      localStorage.setItem('flashCards', JSON.stringify(flashCards))
+      e.target.reset()
+    }
+  }
+  handleChange(e) {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
+  }
+  render() {
+    return (
+      <div className="d-flex justify-content-center">
+        <form
+          onSubmit={ this.handleSave }
+          id="createFlash"
+          className="bg-light rounded mt-5 p-4"
+          style={styles.background}>
+          <h2 className="d-flex justify-content-center">Create a Flash Card</h2>
+          <div className="form-group">
+            <label>Question</label>
+            <input
+              onChange={ this.handleChange }
+              name="question"
+              className="form-control"
+              placeholder="Enter question"></input>
+          </div>
+          <div className="form-group">
+            <label>Answer</label>
+            <input
+              onChange={ this.handleChange }
+              name="answer"
+              className="form-control"
+              placeholder="Enter answer"></input>
+          </div>
+          <div className="d-flex justify-content-center">
+            <button type="submit" className="btn btn-primary">Save</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
