838c9a0f6df4f447ca951185c5c9bcba6e8ef817
Sean Murakami <murakami.sean@yahoo.com>
2018-10-06 20:30:51 -0700

    #6 added arrows to the left and right of the practice card && created an onClick prop that runs a function that updates the currentcard count

--- src/practice.js
+++ src/practice.js
@@ -14,20 +14,43 @@ export default class Practice extends React.Component {
       showAnswer: false
     }
     this.showAnswer = this.showAnswer.bind(this)
+    this.changeCard = this.changeCard.bind(this)
   }
   showAnswer() {
     this.setState({showAnswer: !this.state.showAnswer})
   }
+  changeCard(e) {
+    const target = e.target.id
+    if (target === 'previous') {
+      this.setState(prevState => ({
+        currentCard: prevState.currentCard - 1,
+        showAnswer: false
+      }))
+    }
+    else {
+      this.setState(prevState => ({
+        currentCard: prevState.currentCard + 1,
+        showAnswer: false
+      }))
+    }
+  }
   render() {
+    const { currentCard, showAnswer } = this.state
     if (this.props.flashcards.length > 0) {
-      const answer = this.state.showAnswer ? '' : 'd-none'
-      const buttonDesc = this.state.showAnswer ? 'Hide Answer' : 'Show Answer'
+      const answer = showAnswer ? '' : 'd-none'
+      const buttonDesc = showAnswer ? 'Hide Answer' : 'Show Answer'
       return (
-        <div className="mx-auto mt-5 card" style={ styles.width }>
-          <div className="card-body">
-            <h3 className="mb-0 card-title">{this.props.flashcards[0].question}</h3>
-            <a onClick={this.showAnswer} href="#practice" className="my-3 btn btn-primary btn-sm">{ buttonDesc }</a>
-            <p className={`card-text ${answer}`}>{this.props.flashcards[0].answer}</p>
+        <div className="mt-5">
+          <div className="d-flex justify-content-center align-items-center">
+            <i onClick={ this.changeCard } id="previous" className="mr-2 fas fa-less-than"></i>
+            <div className="card" style={ styles.width }>
+              <div className="card-body">
+                <h3 className="mb-0 card-title">{this.props.flashcards[currentCard].question}</h3>
+                <a onClick={this.showAnswer} href="#practice" className="my-3 btn btn-primary btn-sm">{ buttonDesc }</a>
+                <p className={`card-text ${answer}`}>{this.props.flashcards[currentCard].answer}</p>
+              </div>
+            </div>
+            <i onClick={ this.changeCard } id="next" className="ml-2 fas fa-greater-than"></i>
           </div>
         </div>
       )
