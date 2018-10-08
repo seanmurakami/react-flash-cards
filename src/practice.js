68f90df3a07c08e4c0659a2240355d27b587eb31
Sean Murakami <murakami.sean@yahoo.com>
2018-10-08 12:45:32 -0700

    #7 created two functions, calcNext and calcPrevious that returns an object with the percentage status of the user in the Practice compnonent && updated state to include a status that contains a width property that maintains the width of progress bar

--- src/practice.js
+++ src/practice.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react'
-import ProgressBar from './progress-bar'
 
 const styles = {
   width: {
@@ -14,11 +13,13 @@ export default class Practice extends React.Component {
       currentCard: 0,
       showAnswer: false,
       status: {
-        percentage: '0%'
+        width: '0%'
       }
     }
     this.showAnswer = this.showAnswer.bind(this)
     this.changeCard = this.changeCard.bind(this)
+    this.calcNext = this.calcNext.bind(this)
+    this.calcPrevious = this.calcPrevious.bind(this)
   }
   showAnswer() {
     this.setState({showAnswer: !this.state.showAnswer})
@@ -28,39 +29,59 @@ export default class Practice extends React.Component {
     const { currentCard } = this.state
     if (this.props.flashcards.length - 1 > currentCard) {
       if (target === 'previous' && this.state.currentCard > 0) {
-        this.setState({
-          currentCard: currentCard - 1,
-          showAnswer: false
-        })
+        this.setState(prevState => ({
+          currentCard: prevState.currentCard - 1,
+          showAnswer: false,
+          status: this.calcPrevious(currentCard)
+        }))
       }
       else if (target === 'next') {
-        this.setState({
-          currentCard: currentCard + 1,
-          showAnswer: false
-        })
+        this.setState(prevState => ({
+          currentCard: prevState.currentCard + 1,
+          showAnswer: false,
+          status: this.calcNext(currentCard)
+        }))
       }
     }
     else if (this.props.flashcards.length - 1 === currentCard) {
       if (target === 'previous') {
-        this.setState({
-          currentCard: currentCard - 1,
-          showAnswer: false
-        })
+        this.setState(prevState => ({
+          currentCard: prevState.currentCard - 1,
+          showAnswer: false,
+          status: this.calcPrevious(currentCard)
+        }))
       }
       else {
-        this.setState({currentCard: 0, showAnswer: false})
+        this.setState({currentCard: 0, showAnswer: false, status: {width: '0%'}})
       }
     }
   }
+  calcNext(flashcard) {
+    const status = {
+      width: Math.round((flashcard + 1) / (this.props.flashcards.length) * 100) + '%'
+    }
+    return status
+  }
+  calcPrevious(flashcard) {
+    const status = {
+      width: Math.round((flashcard - 1) / (this.props.flashcards.length) * 100) + '%'
+    }
+    return status
+  }
   render() {
-    const { currentCard, showAnswer } = this.state
+    const { currentCard, showAnswer, status } = this.state
     const { flashcards } = this.props
     const answer = showAnswer ? '' : 'd-none'
     const buttonDesc = showAnswer ? 'Hide Answer' : 'Show Answer'
     if (flashcards.length > 0) {
       return (
         <Fragment>
-          <ProgressBar percentage={ this.state.status }/>
+          <div className="mt-5 d-flex justify-content-center">
+            <div className="progress w-50">
+              <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuemin="0" aria-valuemax="100" style={ status }><span>{ status.width }</span>
+              </div>
+            </div>
+          </div>
           <div className="mt-4">
             <div className="d-flex justify-content-center align-items-center">
               <i onClick={ this.changeCard } id="previous" className="mr-2 fas fa-less-than fa-lg"></i>
